f2e2156d4786342fe941deb6bfd43ec111be0d57
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-05-14 21:35:22 +0430

    get items and create new item

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// Book Struct
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct
+type Author struct {
+	FirstName string `json:"name"`
+	LastName  string `json:"lastname"`
+}
+
+var books []Book
+
+// Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	param := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == param["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		log.Fatalln("something went wrong with decode json")
+	}
+	book.ID = strconv.Itoa(rand.Intn(100))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func main() {
+	books = append(books, Book{ID: "1", Isbn: "124864", Title: "book 1", Author: &Author{FirstName: "john", LastName: "steven"}})
+	books = append(books, Book{ID: "2", Isbn: "548618", Title: "book 2", Author: &Author{FirstName: "dave", LastName: "dao"}})
+	books = append(books, Book{ID: "3", Isbn: "355482", Title: "book 3", Author: &Author{FirstName: "sara", LastName: "ross"}})
+
+	//init router
+	r := mux.NewRouter()
+
+	// Router Handlers
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/book", createBook).Methods("POST")
+	// r.HandleFunc("api/books/{id}", updateBook).Methods("PUT")
+	// r.HandleFunc("api/books/{id}", deleteBooks).Methods("DELETE")
+
+	// Run Server on port :8000
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
